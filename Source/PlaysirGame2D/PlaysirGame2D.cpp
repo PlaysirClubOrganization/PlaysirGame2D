a6bb31329bbddf97f1d91d8dbaed2c967b327c1c
Karkariy <helali44@hotmail.fr>
2016-01-29 23:59:29 +0100

    initialisation of the prject

---
+++ Source/PlaysirGame2D/PlaysirGame2D.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "PlaysirGame2D.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PlaysirGame2D, "PlaysirGame2D" );
+ 
